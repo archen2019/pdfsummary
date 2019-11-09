52f647e325b5b09a7ae9f4b287e2e23857cddedc
Andrew Chen <archen2019@gmail.com>
2019-11-09 14:00:43 -0500

    Improved tesseract result.

--- pdf2text.py
+++ pdf2text.py
@@ -1,6 +1,8 @@
 import subprocess
 from pdf2image import convert_from_path
 from PIL import Image
+import pandas as pd
+import numpy as np
 
 pdf = input('File name: ')
 
@@ -8,10 +10,12 @@ images = convert_from_path(pdf)
 txtlist = []
 
 for img in images:
+    img = img.convert('LA')
     img.save('cur_img.png')
     subprocess.run(['tesseract', 'cur_img.png', 'cur_txt', '--dpi', '125'])
     with open('cur_txt.txt', 'r') as curfile:
         txtlist.extend(curfile.readlines())
+        del txtlist[-1]
         txtlist.append('\n')
 
 with open('fulltext.txt', 'w') as f:
