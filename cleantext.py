3a092c01f93a6bf91ee5db22cd8fb3a006660cbb
Andrew Chen <archen2019@gmail.com>
2019-11-09 14:54:04 -0500

    C C Fixed hyphenation in cleantext.

--- cleantext.py
+++ cleantext.py
@@ -7,3 +7,16 @@ with open('fulltext.txt', 'r') as f:
 
 with open('cleantext.txt', 'w') as f:
     f.write(''.join(newlines))
+
+lines = []
+with open('cleantext.txt', 'r') as f:
+    lines = f.readlines()
+    for i in range(len(lines) - 1):
+        if lines[i][:-1].endswith('-') and lines[i + 1].strip():
+            lines[i] = lines[i][:-2]
+            split = lines[i + 1].split(' ', 1)
+            lines[i] = lines[i] + split[0] + '\n'
+            lines[i + 1] = split[1]
+
+with open('cleantext.txt', 'w') as f:
+    f.write(''.join(lines))
