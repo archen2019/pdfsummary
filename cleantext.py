4d1bc77317a7bcfe3f3245d7ce2c77b5f6d3677e
howard-yen <47925471+howard-yen@users.noreply.github.com>
2019-11-09 15:07:45 -0500

    Changed to hyphen replacement method to using regex.

--- cleantext.py
+++ cleantext.py
@@ -1,23 +1,17 @@
+import re
+
 newlines = []
-with open('fulltext.txt', 'r') as f:
+with open('fulltext.txt', 'r', encoding = 'utf-8') as f:
     lines = f.readlines()
     for i in range(len(lines) - 1):
         if lines[i].strip() or lines[i + 1].strip():
             newlines.append(lines[i])
 
-with open('cleantext.txt', 'w') as f:
-    f.write(''.join(newlines))
-
 # replace hyphenated words with full words
-lines = []
-with open('cleantext.txt', 'r') as f:
-    lines = f.readlines()
-    for i in range(len(lines) - 1):
-        if lines[i][:-1].endswith('-') and lines[i + 1].strip():
-            lines[i] = lines[i][:-2]
-            split = lines[i + 1].split(' ', 1)
-            lines[i] = lines[i] + split[0] + '\n'
-            lines[i + 1] = split[1]
+text = ''.join(newlines)
+def replaceHyphen(match):
+    return re.sub(r'-\s*', '', match.string)
+cleantext = re.sub(r'[a-zA-Z]+-\s*[a-zA-Z]+', replaceHyphen, text, flags=re.IGNORECASE)
 
-with open('cleantext.txt', 'w') as f:
-    f.write(''.join(lines))
+with open('cleantext.txt', 'w', encoding = 'utf-8') as f:
+    f.write(cleantext)
